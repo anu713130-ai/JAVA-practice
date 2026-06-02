7c13eb94cc5e634f96cda8c82aefc9edd74fa7b8
Anupama Modak <anu713130@gmail.com>
2026-06-02 21:00:47 +0530

    Added new project

---
+++ Basic/prime.java
@@ -0,0 +1,30 @@
+package Basic;
+import java.util.Scanner;
+public class prime {
+    public static void main(String[] args) {
+     Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int num=sc.nextInt();
+        boolean isprime=true;
+        if(num<=1)
+            isprime=false;
+        else{
+                for(int i=2;i<num;i++)
+                {
+                    if(num%i==0)
+                    {
+                    isprime=false;
+                    break;
+                    }
+                }
+            }
+        if(isprime)
+        {
+            System.out.println(num+" is prime:");
+        }
+        else
+        {
+            System.out.println(num+" is not prime:");
+        }
+    }
+}
