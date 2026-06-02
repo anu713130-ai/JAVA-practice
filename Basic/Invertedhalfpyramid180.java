c37b1fab3df02e46c0e0faba6e9a48caa5d55b31
Anupama Modak <anu713130@gmail.com>
2026-06-02 10:10:07 +0530

    update new programs

---
+++ Basic/Invertedhalfpyramid180.java
@@ -0,0 +1,21 @@
+package Basic;
+import java.util.Scanner;
+public class Invertedhalfpyramid180 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number of r:");
+        int r=sc.nextInt();
+        for(int i=1;i<=r;i++)
+        {
+            for(int j=1;j<=r-i;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
