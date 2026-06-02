191d53fed382bd43cf24adfd1544a3bbfdc3f274
Anupama Modak <anu713130@gmail.com>
2026-06-02 20:31:00 +0530

    Added new project

---
+++ Basic/Reversenumber.java
@@ -0,0 +1,17 @@
+package Basic;
+import java.util.Scanner;
+public class Reversenumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int num=sc.nextInt();
+        int rev=0;
+        while(num!=0)
+        {
+            int digit=num%10;
+            rev=rev*10+digit;
+            num=num/10;
+        }
+        System.out.print("Reverse of the number is:"+rev);
+    }
+}
