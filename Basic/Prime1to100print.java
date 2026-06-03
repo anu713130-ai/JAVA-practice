4ee93679b46519f123751a16ce346b0a8a5aaf8f
Anupama Modak <anu713130@gmail.com>
2026-06-03 12:11:04 +0530

    Added new project

---
+++ Basic/Prime1to100print.java
@@ -0,0 +1,24 @@
+package Basic;
+import java.util.Scanner;
+public class Prime1to100print {
+    public static void main(String[] args) {
+        int num;
+        for(num=2;num<=100;num++)
+        {
+        boolean isprime=true;
+        for(int i=2;i<num;i++)
+        {
+            if(num%i==0)
+            {
+            isprime=false;
+            break;
+            }
+        }
+        if(isprime)
+        {
+         System.out.println(num+" is prime");
+        }
+    }
+    }
+}
+
