22a490ae9be58c41d0967cee73e1ffb7df433d7f
Anupama Modak <anu713130@gmail.com>
2026-06-03 20:15:59 +0530

    Added new program

---
+++ NumberbasedQ/GcdorHcfof2no.java
@@ -0,0 +1,20 @@
+package NumberbasedQ;
+import java.util.Scanner;
+public class GcdorHcfof2no {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the 1st number:");
+        int num1=sc.nextInt();
+        System.out.println("Enter the 2nd number:");
+        int num2=sc.nextInt();
+        int gcd=1;
+        for(int i=1;i<=num1&&i<=num2;i++)
+        {
+            if(num1%i==0&&num2%i==0)
+            {
+                gcd=i;
+            }
+        }
+        System.out.println("Gcd of this two numbers:"+gcd);
+    }
+}
