aba49a4c54e815cec119baa1bc678373682bcc42
Anupama Modak <anu713130@gmail.com>
2026-06-04 09:41:38 +0530

    Added new program

---
+++ Pattern/Floydstriangle.java
@@ -0,0 +1,19 @@
+package Pattern;
+import java.util.Scanner;
+public class Floydstriangle {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number of rows:");
+        int r=sc.nextInt();
+        int number=1;
+        for(int i=1;i<=r;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(number+" ");
+                number++;
+            }
+            System.out.println();
+        }
+    }
+}
