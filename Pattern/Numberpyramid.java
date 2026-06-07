c4fedf555cc61b9316a1d0192895a093881601b3
Anupama Modak <anu713130@gmail.com>
2026-06-07 10:29:02 +0530

    Added new program

---
+++ Pattern/Numberpyramid.java
@@ -0,0 +1,21 @@
+package Pattern;
+import java.util.Scanner;
+public class Numberpyramid {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the row:");
+        int r=sc.nextInt();
+        for(int i=1;i<=r;i++)
+        {
+            for(int j=1;j<=r-i;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
