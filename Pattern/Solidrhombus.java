f6ea3c937b39d1bd8fd20220198431730cfaf80f
Anupama Modak <anu713130@gmail.com>
2026-06-07 10:11:32 +0530

    Added new program

---
+++ Pattern/Solidrhombus.java
@@ -0,0 +1,21 @@
+package Pattern;
+import java.util.Scanner;
+public class Solidrhombus {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the row:");
+        int r=sc.nextInt();
+        for(int i=1;i<=r;i++)
+        {
+            for(int j=1;j<=r-i;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=r;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
